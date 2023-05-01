6368f41d454d637956299b88a7a6f9877786a283
Shigo97 <126001345+Shigo97@users.noreply.github.com>
2023-05-01 01:54:26 +0100

    Add files via upload

---
+++ Objective and descrpition of the internship @cei.py
@@ -0,0 +1,24 @@
+'''
+goal - given these all features, how do we identify whether this person is of a poor category and requires subsidy"
+determined by level 1,2,3,4
+
+do null handling to avoid ineffectiveness in ML
+download anaconda and latest python - and go to the terminal window or open the jupiter notebook
+you can save the file as python file or save as an html file
+
+write various generic functions -
+
+don't go for write a function first but go for the print 
+
+
+various features and att related to the household in data sets 'train'
+idhogar is the head of the household
+
+target column-
+
+
+merge 2 data sets whereever the target has no data will be null
+test and train are different data sets but have some similar data as well
+test data set:
+
+'''
